65273e6433c97fd7539323097bffc8f42cde2c9e
Shravan Purwar <sravanpurwar@gmail.com>
2023-10-19 06:57:38 +0530

    Update 1_Price Predictor.py

--- pages/1_Price Predictor.py
+++ pages/1_Price Predictor.py
@@ -7,10 +7,10 @@ st.set_page_config(page_title="Viz Demo")
 
 
 
-with open('cosine_sim1','rb') as file:
+with open('cosine_sim1.pkl','rb') as file:
     df = pickle.load(file)
 
-with open('cosine_sim2','rb') as file:
+with open('cosine_sim2.pkl','rb') as file:
     pipeline = pickle.load(file)
 
 
